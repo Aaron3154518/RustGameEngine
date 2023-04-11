cd1f1d475ea7c5a25783b71094f63f7d74fb6698
Aaron3154518 <aaoren210@gmail.com>
2023-04-10 21:40:04 -0400

    Added memory management structs for window, renderer, and texture

--- src/main.rs
+++ src/main.rs
@@ -14,36 +14,24 @@ mod sdl2_image {
     include!(concat!(env!("OUT_DIR"), "/sdl2_image_bindings.rs"));
 }
 
-use std::{ffi::CString, mem};
+use std::mem;
+
+mod pointers;
+use pointers::*;
 
 use num_traits::FromPrimitive;
 
-fn get_texture(r: *mut sdl2::SDL_Renderer, file: &str) -> *mut sdl2::SDL_Texture {
-    let cstr = CString::new(file).expect("Failed to creat CString");
-    unsafe { sdl2_image::IMG_LoadTexture(r, cstr.as_ptr()) }
-}
 fn main() {
     unsafe {
         // Initialize SDL2
         let sdl_init = sdl2::SDL_Init(sdl2::SDL_INIT_EVERYTHING);
-        let img_init = 0;
+        let img_init = sdl2_image::IMG_Init(sdl2_image::IMG_InitFlags::IMG_INIT_PNG as i32);
 
         // Create a window
-        let window = sdl2::SDL_CreateWindow(
-            "My SDL2 Window\0".as_ptr() as *const i8,
-            sdl2::SDL_WINDOWPOS_CENTERED_MASK as i32,
-            sdl2::SDL_WINDOWPOS_CENTERED_MASK as i32,
-            640,
-            480,
-            sdl2::SDL_WindowFlags::SDL_WINDOW_SHOWN as u32,
-        );
+        let window = Window::new().title("Game Engine").dimensions(960, 720);
 
-        let renderer = sdl2::SDL_CreateRenderer(window, -1, 0);
-        assert!(!renderer.is_null(), "Failed to create renderer");
-        let tex = get_texture(renderer, "res/bra_vector.png");
-        if tex.is_null() {
-            eprintln!("Failed to load image: {:?}", sdl2::SDL_GetError());
-        }
+        let renderer = Renderer::new(&window);
+        let tex = Texture::new(&renderer, "res/bra_vector.png");
 
         // Wait for a key press
         sdl2::SDL_EventState(
@@ -62,8 +50,7 @@ fn main() {
                 _ => (),
             }
 
-            sdl2::SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            sdl2::SDL_RenderClear(renderer);
+            renderer.clear();
 
             // Draw the texture to the center of the screen
             let mut dest_rect = sdl2::SDL_Rect {
@@ -72,15 +59,13 @@ fn main() {
                 w: 100 as i32,
                 h: 100 as i32,
             };
-            sdl2::SDL_RenderCopy(renderer, tex, std::ptr::null(), &mut dest_rect);
+            tex.draw(&renderer, std::ptr::null(), &mut dest_rect);
 
             // Update the screen
-            sdl2::SDL_RenderPresent(renderer);
+            renderer.present();
         }
 
         // Destroy the window and quit SDL2
-        sdl2::SDL_DestroyTexture(tex);
-        sdl2::SDL_DestroyWindow(window);
         sdl2_image::IMG_Quit();
         sdl2::SDL_Quit();
     }
